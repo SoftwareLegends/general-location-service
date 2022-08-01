1e560369a98e9dce07d4831335d1ed25829d977a
Al-Taie <agprosup@gmail.com>
2022-08-01 11:18:37 +0300

    NPI-1561 adding google in LocationRequestProvider.kt

--- GLSLibrary/src/main/java/com/gateway/glslibrary/utils/LocationRequestProvider.kt
+++ GLSLibrary/src/main/java/com/gateway/glslibrary/utils/LocationRequestProvider.kt
@@ -1,9 +1,19 @@
 package com.gateway.glslibrary.utils
 
 
+import com.google.android.gms.location.Priority
+import com.google.android.gms.location.LocationRequest as LocationRequestGoogle
 import com.huawei.hms.location.LocationRequest as LocationRequestHuawei
 
 sealed class LocationRequestProvider<out T> {
+    object Google : LocationRequestProvider<LocationRequestGoogle>() {
+        val locationRequest: LocationRequestGoogle = LocationRequestGoogle.create().apply {
+            priority = Priority.PRIORITY_HIGH_ACCURACY
+            interval = Constant.UPDATE_INTERVAL_IN_MILLISECONDS
+            fastestInterval = Constant.FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS
+            numUpdates = Constant.NUMBER_OF_UPDATES
+        }
+    }
 
     object Huawei : LocationRequestProvider<LocationRequestHuawei>() {
         val locationRequest: LocationRequestHuawei = LocationRequestHuawei.create().apply {
