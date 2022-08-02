fe5cc616465912e57125397ac67eb7793cdb4d43
Al-Taie <agprosup@gmail.com>
2022-08-02 11:18:07 +0300

    NPI-1561 implement NoneService.kt functions

--- GLSLibrary/src/main/java/com/gateway/glslibrary/data/NoneService.kt
+++ GLSLibrary/src/main/java/com/gateway/glslibrary/data/NoneService.kt
@@ -5,17 +5,17 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.IntentSenderRequest
 import com.gateway.glslibrary.domain.Resource
 import com.gateway.glslibrary.domain.interfaces.LocationService
+import com.gateway.glslibrary.utils.enums.LocationFailure
+import com.gateway.glslibrary.utils.extenstions.toModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
-class NoneService: LocationService {
-    override suspend fun lastLocation(): Resource<Location> {
-        TODO("Not yet implemented")
-    }
+class NoneService : LocationService {
+    override suspend fun lastLocation(): Resource<Location> =
+        Resource.Fail(data = LocationFailure.LOCATION_SERVICE_NOT_FOUND.toModel())
 
-    override suspend fun getCurrentLocation(): Resource<Location> {
-        TODO("Not yet implemented")
-    }
+    override fun requestLocationUpdates(): Flow<Resource<Location>> =
+        flow { emit(Resource.Fail(data = LocationFailure.LOCATION_SERVICE_NOT_FOUND.toModel())) }
 
-    override fun locationSettings(resultContracts: ActivityResultLauncher<IntentSenderRequest>) {
-        TODO("Not yet implemented")
-    }
+    override fun locationSettings(resultContracts: ActivityResultLauncher<IntentSenderRequest>) {}
 }
--- GLSLibrary/src/main/java/com/gateway/glslibrary/utils/enums/LocationFailure.kt
+++ GLSLibrary/src/main/java/com/gateway/glslibrary/utils/enums/LocationFailure.kt
@@ -6,5 +6,6 @@ enum class LocationFailure(
 ) {
     GPS_PROVIDER_IS_DISABLED("Gps provider is disabled, try to enable it", 200),
     LOCATION_NEVER_RECORDED("Location has never been recorded on this device", 201),
-    NETWORK_UNAVAILABLE("Network is unavailable, location can not be requested", 202)
+    NETWORK_UNAVAILABLE("Network is unavailable, location can not be requested", 202),
+    LOCATION_SERVICE_NOT_FOUND("Location service not found", 404)
 }
