f961db391a8d405b608ed4ca2df69d3d5deee7ec
Al-Taie <agprosup@gmail.com>
2022-08-04 16:22:45 +0300

    NPI-1561 remove enum classes

--- gls/src/main/java/com/gateway/gls/utils/extenstions/extEnum.kt
+++
@@ -1,6 +0,0 @@
-package com.gateway.gls.utils.extenstions
-
-import com.gateway.gls.domain.models.Error
-import com.gateway.gls.utils.enums.LocationFailure
-
-fun LocationFailure.toModel() = Error(message = message, code = code)
