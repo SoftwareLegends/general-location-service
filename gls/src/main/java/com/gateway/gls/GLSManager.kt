a5c2aeffd11984882cbd9d1838c5fc7935c99634
Al-Taie <Ahmed.m@the-gw.com>
2022-10-20 15:29:17 +0300

    NP-957 create GLSManager

---
+++ gls/src/main/java/com/gateway/gls/GLSManager.kt
@@ -0,0 +1,49 @@
+package com.gateway.gls
+
+import android.location.Location
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.IntentSenderRequest
+import com.gateway.core.base.Resource
+import com.gateway.gls.di.GLSModule.repository
+import com.gateway.gls.domain.interfaces.LocationRepository
+import com.gateway.gls.domain.entities.Priority
+import com.gateway.gls.domain.entities.Services
+import com.gateway.gls.services.ServiceAvailability
+import kotlinx.coroutines.flow.Flow
+
+object GLSManager : LocationRepository {
+    override fun lastLocationAsFlow(): Flow<Resource<Location>> =
+        repository.lastLocationAsFlow()
+
+    override suspend fun lastLocation(): Resource<Location> =
+        repository.lastLocation()
+
+    override fun requestLocationUpdatesAsFlow(): Flow<Resource<Location>> =
+        repository.requestLocationUpdatesAsFlow()
+
+    override suspend fun requestLocationUpdates(): Resource<List<Location>> =
+        repository.requestLocationUpdates()
+
+    override fun configureLocationRequest(
+        priority: Priority,
+        intervalMillis: Long,
+        minUpdateIntervalMillis: Long,
+        maxUpdates: Int,
+        maxUpdateDelayMillis: Long
+    ) = repository.configureLocationRequest(
+        priority = priority,
+        intervalMillis = intervalMillis,
+        minUpdateIntervalMillis = minUpdateIntervalMillis,
+        maxUpdates = maxUpdates,
+        maxUpdateDelayMillis = maxUpdateDelayMillis
+    )
+
+    override fun requestLocationSettings(resultContracts: ActivityResultLauncher<IntentSenderRequest>) =
+        repository.requestLocationSettings(resultContracts)
+
+    override val isLocationServicesAvailable: Boolean
+        get() = repository.isLocationServicesAvailable
+
+    val serviceProvider: Services
+        get() = ServiceAvailability.serviceProvider
+}
